b6c2b9e430001fa09c2d5a996710ee9f0219e82d
tindrew <tindrewmar@gmail.com>
2022-10-26 19:17:31 -0500

    initial commit

---
+++ lib/sortimg.ex
@@ -0,0 +1,18 @@
+defmodule Sortimg do
+  @moduledoc """
+  Documentation for `Sortimg`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Sortimg.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sortimg_test.exs
@@ -0,0 +1,8 @@
+defmodule SortimgTest do
+  use ExUnit.Case
+  doctest Sortimg
+
+  test "greets the world" do
+    assert Sortimg.hello() == :world
+  end
+end
