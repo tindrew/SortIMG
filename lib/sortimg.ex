ceed23ba0153c933ef325ea7585109eb289bab4a
tindrew <tindrewmar@gmail.com>
2022-11-01 10:31:38 -0500

    add functionality to delve into directories and return files in a flat list

--- lib/sortimg.ex
+++ lib/sortimg.ex
@@ -13,7 +13,20 @@ defmodule Sortimg do
 
   """
   def get_files(directory) do
-    File.ls!(directory)
+    files_and_directories = File.ls!(directory)
+
+    {directories, files} = Enum.split_with(files_and_directories, fn file_or_directory ->
+        File.dir?(file_or_directory)
+    end)
+
+    ### what do we want to return?
+    Enum.map(files, fn file -> Path.join(directory, file) end) ++ Enum.flat_map(directories, fn directory -> get_files(directory) end)
+
   end
 
 end
+
+[dir | _] = System.argv()
+
+Sortimg.get_files(dir)
+|> IO.inspect()
--- test/sortimg_test.exs
+++ test/sortimg_test.exs
@@ -3,6 +3,6 @@ defmodule SortimgTest do
   doctest Sortimg
 
   test "should get files" do
-    assert Sortimg.get_files("./test") == "sortimg_test.exs", "test_helper.exs"
+  assert Sortimg.get_files("./test") == ["test_helper.exs", "sortimg_test.exs"]
   end
 end
