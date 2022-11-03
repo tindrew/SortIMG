f7526cd3a0b40d69477ffc11a2a2f768272feee8
tindrew <tindrewmar@gmail.com>
2022-11-03 00:25:36 -0500

    added subpaths to get_files

--- lib/sortimg.ex
+++ lib/sortimg.ex
@@ -15,15 +15,29 @@ defmodule Sortimg do
   def get_files(directory) do
     files_and_directories = File.ls!(directory)
 
-    {directories, files} = Enum.split_with(files_and_directories, fn file_or_directory ->
+    {directories, files} =
+      Enum.split_with(files_and_directories, fn file_or_directory ->
         File.dir?(file_or_directory)
-    end)
+      end)
 
     ### what do we want to return?
-    Enum.map(files, fn file -> Path.join(directory, file) end) ++ Enum.flat_map(directories, fn directory -> get_files(directory) end)
-
+    Enum.map(files, fn file -> Path.join(directory, file) end) ++
+      Enum.map(
+        Enum.flat_map(directories, fn directory -> get_files(directory) end),
+        fn subpath -> Path.join(directory, subpath) end
+      )
   end
 
+  def identify_files(files) do
+    Enum.filter(files, fn file -> is_file_jpeg?(file) end)
+  end
+
+  def is_file_jpeg?(file) do
+#learn how to open a file
+#jpeg's magic number is ff d8 ff
+#my assignment pattern matching find_strings_starting_with_fgh = ["fgh", "hgf", "ffe", "tfgh", "fghh", "ffgh"]
+#can't use String.starts_with
+  end
 end
 
 [dir | _] = System.argv()
--- test/sortimg_test.exs
+++ test/sortimg_test.exs
@@ -3,6 +3,6 @@ defmodule SortimgTest do
   doctest Sortimg
 
   test "should get files" do
-  assert Sortimg.get_files("./test") == ["test_helper.exs", "sortimg_test.exs"]
+    assert Sortimg.get_files("./test") == ["test_helper.exs", "sortimg_test.exs"]
   end
 end
