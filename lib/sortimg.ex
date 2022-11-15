27e6b4c7034c5592fa944a6d084a0c4467683b2a
tindrew <tindrewmar@gmail.com>
2022-11-15 00:52:45 -0600

    add is_file_jpeg function

--- lib/sortimg.ex
+++ lib/sortimg.ex
@@ -33,14 +33,33 @@ defmodule Sortimg do
   end
 
   def is_file_jpeg?(file) do
-#learn how to open a file
-#jpeg's magic number is ff d8 ff
-#my assignment pattern matching find_strings_starting_with_fgh = ["fgh", "hgf", "ffe", "tfgh", "fghh", "ffgh"]
-#can't use String.starts_with
+    # give it a file
+    # open file
+    # get some bytes from the file (aka: get some of the file as a binary)
+    # so, lets get 25
+
+    # check if magic number exists
+    # %File.Stream
+    #elixir lib/sortimg.ex .
+    case File.read(file) do
+      {:ok, <<0xFF, 0xD8, 0xFF, _::binary>>} -> true
+      _ -> false
+    end
+
+#tuples, one as pngs one as jpegs, one as ??
+
+    # https://github.com/ishikawa/elixir-magic-number
+    # scan the file, if magic_number == "ff d8 ff"
+    # read the file's binary signature?
+    # learn how to open a file
+    # jpeg's magic number is ff d8 ff
+    # my assignment pattern matching find_strings_starting_with_fgh = ["fgh", "hgf", "ffe", "tfgh", "fghh", "ffgh"]
+    # can't use String.starts_with
   end
 end
 
 [dir | _] = System.argv()
 
 Sortimg.get_files(dir)
+|> Sortimg.identify_files()
 |> IO.inspect()
